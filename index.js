b1ad074dd23214a557ad4669ef22f85cceb2ac6f
허지혜 <141692224+zihyeeee@users.noreply.github.com>
2025-02-26 23:14:01 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -24,48 +24,121 @@ $(function(){
 
     // swiper
     var mySwiper = new Swiper('.swiper-container', {
-        direction: 'horizontal', // 방향
-        slidesPerView: 6, // 슬라이드를 한번에 3개를 보여준다
-        slidesPerGroup : 1, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
-        spaceBetween: 60, // 슬라이드간 padding 값 30px 씩 떨어뜨려줌
-        
-        // 그룹수가 맞지 않을 경우 빈칸으로 메우기
-        // 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
+        direction: 'horizontal', 
+        slidesPerView: 6,
+        slidesPerGroup : 1, // 그룹으로 묶을 수
+        spaceBetween: 60, // 슬라이드간 padding 값
+
         loopFillGroupWithBlank : false,
 
         loop: true,
         loopAdditionalSlides: 1,
-         // loop 를 true 로 할경우 무한반복 슬라이드, false 로 할경우 슬라이드의 끝에서 더보여지지 않음
         autoplay:{delay: 4000},
 
-        // If we need pagination 
         pagination: {
             el: '.swiper-pagination',
             clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
             type: 'progressbar', // And if we need scrollbar 진행바
         },
         
-        // Navigation arrows 
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         }
         
-        })
+    })
 
-        // header        
-        $(window).scroll(function(){
-            let winTop = $(window).scrollTop();
-            let headerTop = $('#header').offset().top;
-            console.log(headerTop)
+    // header        
+    $(window).scroll(function(){
+        let winTop = $(window).scrollTop();
 
-            if(winTop >= 0) {
-                $('#header').css('position','fixed');
-                $('#header').css('top','0');
-            } else {
-                $('#header').css('position','relative');
-            }
-        })
+        if(winTop >= 0) {
+            $('#header').css('position','fixed');
+            $('#header').css('top','0');
+        } else {
+            $('#header').css('position','relative');
+        }
+    })
 
+    //con01 deco
+    $(window).scroll(function(){
+        let winTop = $(window).scrollTop();
+        let con01 = $('.con01').offset().top;
+
+        let isAnimated = $('.con01_deco1').data('animated');
+
+        if(winTop >= con01-340 && !isAnimated) {
+            $('.con01_deco1').data('animated', true);
+            $('.con01_deco1').animate({left:'43%'},400)
+                             .animate({width:'10%'},400);
+            $('.con01_deco2').animate({right:'43%'},400)
+                             .animate({width:'8%'},400);
+        } 
+    })
+
+    //con03
+    $('.tab').click(function(){
+        let vid = $(".wrap video");
+
+        // vid[0].pause();
+        vid.css("display","none")
+        $('.tab').removeClass('on');            
+        $(this).addClass('on');            
+        $('.con03 .wrap').removeClass('on');            
+        $(this).next().addClass('on');
+    })
+
+    $('.wrap img.paly_btn').click(function(e){
+        e.preventDefault();
+        $(this).next().css("display","block");
+        $(this).next().play();
+    })
+
+    function vid_play (tab_num, vid_url){
+        $('.wrap video').attr('src','');
+        $('.wrap'+tab_num).find('video').attr('src',vid_url)
+    }
+
+    //agree
+    $('#agree_all').change(function(){
+        $('.agree_ck').prop('checked', $(this).is(':checked'))
+    })
+
+    $('.agree_ck').change(function(){
+        if($('.agree_ck:checked').length == 2){
+            $('#agree_all').prop('checked', true);
+        } else {
+            $('#agree_all').prop('checked', false);
+        }
+    })
+
+    close();
+    sign();
+
+    $('.bg').click(function(){
+        $(this).parent().fadeOut();
+    })
 });
 
+function sign() {
+    let sign_btn = document.getElementsByClassName('sign_btn')[0];
+    if(sign_btn) {
+        sign_btn.addEventListener('click', function() {
+            const pop1 = document.getElementsByClassName('sign_wrap')[0];
+            pop1.style.display = 'block'
+        })
+    }
+}
+
+function close() {
+    let close_btn = document.getElementsByClassName('close')[0];
+    if(close_btn) {
+        close_btn.addEventListener('click', function(){
+            const parentElement = close_btn.parentElement;
+            const grandParentElement = parentElement ? parentElement.parentElement : null;
+            if(grandParentElement) {
+                grandParentElement.style.display = 'none';
+            }
+        })
+    }
+}
